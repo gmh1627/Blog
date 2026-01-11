ccdbdd6c98c439ed5b28f86c30935ac94d00f9fe
gmh1627 <3558965271@qq.com>
2026-01-11 22:42:23 +0800

    commit

--- draw.py
+++ draw.py
@@ -7,8 +7,8 @@ rcParams['axes.unicode_minus'] = False   # 解决负号显示问题
 
 # 数据
 years = ["大学前", "2022-2023", "2024", "2025", "2026"]
-new_cities = [19, 2, 21, 21, 2]  # 新到的城市
-cumulative_cities = [19, 21, 42, 63, 65]  # 累计去过的城市
+new_cities = [19, 2, 21, 21, 3]  # 新到的城市
+cumulative_cities = [19, 21, 42, 63, 66]  # 累计去过的城市
 
 # 创建图形并绘制新到的城市折线图
 plt.figure(figsize=(10, 6))
